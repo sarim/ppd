ff9e1d7c6953b255060e5e8818a1d8a21e4f3986
sarim <sarim2005@gmail.com>
2013-04-29 14:07:11 +0600

    Running pv|dd and Capturing output Works

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,11 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     this->devices = new QMap<QString, QString>();
+    this->dd_process = new QProcess(this);
+    //this->dd_process->setProcessChannelMode(QProcess::ForwardedChannels);
+    connect(this->dd_process,SIGNAL(readyReadStandardError()),this,SLOT(when_receive_stdout()));
+    connect(this->dd_process,SIGNAL(finished(int,QProcess::ExitStatus)),this,SLOT(when_process_finished(int, QProcess::ExitStatus)));
+
     refresh_devices();
 
 }
@@ -120,13 +125,44 @@ QString MainWindow::run_and_get_stdout(QStringList arguments){
 
 void MainWindow::update_task_list(){
     QString task_string = "Will Write " + ui->lineEdit->text() + " to " + this->devices->key( ui->comboBox->currentText());
-
+    this->toogle_ui_lock(false);
     ui->lbl_task_list->setText(task_string);
+
+}
+
+void MainWindow::run_async_process(QString cmd){
+    QStringList sh_arg;
+    sh_arg << "-c" << cmd;
+    this->dd_process->start("/bin/sh",sh_arg);
+}
+
+void MainWindow::toogle_ui_lock(bool islock){
+    ui->pushButton->setEnabled(!islock);
+    ui->pushButton_2->setEnabled(!islock);
+    ui->pushButton_4->setEnabled(!islock);
+    ui->pushButton_5->setEnabled(!islock);
+    ui->comboBox->setEnabled(!islock);
+    ui->checkBox->setEnabled(!islock);
+
+    if (!islock && !(ui->lineEdit->text().length() > 0 && ui->comboBox->currentText().length() > 0)){
+        ui->pushButton_2->setEnabled(false);
+        ui->pushButton_5->setEnabled(false);
+
+    }
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::when_process_finished( int exitCode, QProcess::ExitStatus exitStatus){
+    qDebug() << this->dd_process->readAllStandardError();
+    qDebug() << this->dd_process->readAllStandardOutput();
+    ui->lbl_task_list->setText("All Done");
+    this->toogle_ui_lock(false);
 }
 
 void MainWindow::on_pushButton_3_clicked()
@@ -160,4 +196,36 @@ void MainWindow::on_comboBox_currentIndexChanged(const QString &arg1)
 void MainWindow::on_checkBox_stateChanged(int arg1)
 {
     this->refresh_devices();
+}
+
+void MainWindow::when_receive_stdout(){
+    QString progress_str = QString(this->dd_process->readAllStandardError());
+    QList<QStringList> progress_vars_list = this->regex_helper("(\\d+:\\d+:\\d+)\\s\\[([^\\]]+)\\].* (\\d+)\\%(.+ETA.+\\d+:\\d+:\\d+)?",progress_str);
+    QString eta_status_str = "Time Elapsed : %1        Estimated Time Remaining : %2          %3";
+    if (progress_vars_list.size() > 0){
+        QStringList progress_vars = progress_vars_list[0];
+        progress_vars.pop_front();
+        qDebug() << progress_vars;
+        ui->progressBar->setValue(progress_vars[2].toInt());
+
+        QString time_e = progress_vars[0].trimmed();
+        QString speed_p_s = progress_vars[1].trimmed();
+        QString time_eta = progress_vars[3].contains("ETA") ? progress_vars[3].trimmed() : "0:00:00";
+
+        eta_status_str = eta_status_str.arg(time_e,time_eta,speed_p_s);
+
+        ui->lbl_eta_status->setText(eta_status_str);
+
+    }
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    //write to device
+
+    QString dd_command = QString("pv -f < \"%1\" | dd of=\"%2\" bs=2m").arg(ui->lineEdit->text(), this->devices->key( ui->comboBox->currentText()));
+
+    this->toogle_ui_lock(true);
+
+    this->run_async_process(dd_command);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -34,16 +34,25 @@ private slots:
 
     void on_checkBox_stateChanged(int arg1);
 
+    void when_receive_stdout();
+
+    void when_process_finished( int exitCode, QProcess::ExitStatus exitStatus );
+
+    void on_pushButton_2_clicked();
+
 private:
     Ui::MainWindow *ui;
-    QMap<QString, QString> *devices;
     void refresh_devices();
     void refresh_devices_win();
     void refresh_devices_mac();
     void refresh_devices_nix();
     void update_task_list();
+    void run_async_process(QString cmd);
+    void toogle_ui_lock(bool islock);
+    QMap<QString, QString> *devices;
 	QString run_and_get_stdout(QStringList arguments);
 	QList<QStringList> regex_helper(QString pattern, QString text);
+    QProcess *dd_process;
 
 };
 
